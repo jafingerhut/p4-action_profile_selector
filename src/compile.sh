d9629dac05911b8dd1593996a743bd03dad7b64f
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2020-05-13 03:20:03 -0700

    Add another working example named as2.p4

--- src/compile.sh
+++ src/compile.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 
-set -x
-for j in as1.p4
+for j in *.p4
 do
+    echo "------------------------------------------------------------"
     B=`basename $j .p4`
+    set -x
     p4c --target bmv2 --arch v1model --p4runtime-files $B.p4info.txt $B.p4
+    set +x
 done
